d464d454ad749dd6222e1c30a1cb88322b8de42b
nayfort <mitrofanovvladislav2004@gmail.com>
2023-04-23 22:01:29 +0200

    form

--- client/my-react-app/src/components/NavBar.js
+++ client/my-react-app/src/components/NavBar.js
@@ -1,22 +1,28 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
-import {Container, Nav, Navbar, NavLink} from "react-bootstrap";
+import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
 import {SHOP_ROUTE} from "../utils/consts";
+import {observer} from "mobx-react-lite";
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context)
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color: 'white'}} path={SHOP_ROUTE}>BuyDevice</NavLink>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
+                {user.isAuth ?
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={'outline-light'}>Адмін панель</Button>
+                        <Button variant={'outline-light'} className='m-lg-2'>Увійти</Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Авторизація</Button>
+                    </Nav>
+                }
             </Container>
         </Navbar>
     );
-};
+});
 
 export default NavBar;
--- client/my-react-app/src/pages/Auth.js
+++ client/my-react-app/src/pages/Auth.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Card, Container, Form} from "react-bootstrap";
 
 const Auth = () => {
     return (
-        <div>
-        AUTH
-        </div>
+        <Container
+            className='d-flex justify-content-center align-items-center'
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className='p-5'>
+                <h2 className='m-auto'>Авторизація</h2>
+                <Form className='d-flex flex-column'>
+                <Form.Control
+                    className='mt-3'
+                    placeholder='Введіть ваш email...'
+                />
+                    <Form.Control
+                    className='mt-3'
+                    placeholder='Введіть ваш пароль...'
+                />
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
