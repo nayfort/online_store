ca46ed9022bf8ee5d7e13f11f1871093240f77eb
nayfort <mitrofanovvladislav2004@gmail.com>
2023-04-24 16:45:26 +0200

    auth

--- client/my-react-app/src/pages/Auth.js
+++ client/my-react-app/src/pages/Auth.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, Container, Form} from "react-bootstrap";
+import {Button, Card, Container, Form, NavLink, Row} from "react-bootstrap";
+import {REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
     return (
@@ -10,15 +11,23 @@ const Auth = () => {
             <Card style={{width: 600}} className='p-5'>
                 <h2 className='m-auto'>Авторизація</h2>
                 <Form className='d-flex flex-column'>
-                <Form.Control
-                    className='mt-3'
-                    placeholder='Введіть ваш email...'
-                />
                     <Form.Control
-                    className='mt-3'
-                    placeholder='Введіть ваш пароль...'
-                />
+                        className='mt-3'
+                        placeholder='Введіть ваш email...'
+                    />
+                    <Form.Control
+                        className='mt-3'
+                        placeholder='Введіть ваш пароль...'
+                    />
                 </Form>
+                <Row className='d-flex justify-content-between mt-3 pl-3 pr-3'>
+                    <div>Не маєте акаунт? <NavLink path={REGISTRATION_ROUTE}>Зареєструйтесь!</NavLink></div>
+                    <Button
+                        variant={'outline-success'}
+                    >
+                        Увійти
+                    </Button>
+                </Row>
             </Card>
         </Container>
     );
